545da9bbeb82eeaa35f27d0b1037ef45407fe19c
DanoGod <odano123god@gmail.com>
2021-04-26 12:21:11 -0500

    Final

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "aeiou"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://finalproject:LabLab@localhost:5432/finalproject"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://drxyxwztwolzpf:f68880fe9c79f93bbf43fb1de7cf7b0d8a644947c0e8ae70ab351011283a7e05@ec2-54-224-120-186.compute-1.amazonaws.com:5432/danpbh5i20o9fn'
+HEROKU_POSTGRESQL_AMBER_URL='postgres://tbcmhmpogxxgfw:6913769ad1e0af3491ce12533f120199a244c06317bd81cee15aeb4eba2b4ec4@ec2-3-233-43-103.compute-1.amazonaws.com:5432/ddh5j72ndu04b2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 db = SQLAlchemy(app)
